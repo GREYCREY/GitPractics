35ccd06c6c63fbf2462ebaa935608f63fafb6dbb
GREYCREY <greycrey@yandex.ru>
2025-03-04 14:34:22 +0600

    Добавлена верстка

---
+++ Contacts/View/Model/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    class Contact
+    {
+        private string _name;
+        private string _phoneNumber;
+        private string _email;
+        public string Name { get { return _name; } set { _name = value; } }
+        public string PhoneNumber { get { return _phoneNumber; } set { _phoneNumber = value; } }
+        public string Email { get { return _email; } set { _email = value; } }
+
+    }
+}
